5d856a06085e6a7f1dd576779ee6cc0a46f9ca04
Leandro Santos <liusantos@gmail.com>
2020-03-11 02:56:57 -0300

    App backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path =  require('path'); 
 
 const routes = require('./routes');
 
@@ -20,6 +21,7 @@ mongoose.connect('mongodb+srv://liusantos:E1212285@curso-omnistack-eeegy.mongodb
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
